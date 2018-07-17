222d0393d8d373e6a953c7425e876f1ab844fce8
OrmOrz1012 <OrmOrz1012@gmail.com>
2018-07-17 11:52:04 +0800

    just test

--- blog/1.sh
+++ blog/1.sh
@@ -5,6 +5,8 @@
 # dev run hugo server
 hugo server -b http://localhost:1313 --bind=0.0.0.0
 
+# hugo server -v -D -w -b http://localhost:1313 --bind=0.0.0.0 --disableFastRender
+
 
 # generate static site
 # hugo --baseUrl="http://localhost:1313"     # Generate static sites in the sample-site
